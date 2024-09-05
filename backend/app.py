dd90fe8694233569a86029f0ba5201ff6ea32878
rtintertbs1 <robintinter@gmx.de>
2024-09-05 13:33:41 +0200

    Für Tobias

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,12 @@ from flask import Flask
 from routes.randomnumber import randomnumberBlueprint  
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "überlegdirwas"
+
+
+#eigene route für session handling?  
 
 app.register_blueprint(randomnumberBlueprint, url_prefix='/randomnumber')
-
-@app.route("/")
-def greet():
-    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/routes/randomnumber.py
+++ backend/routes/randomnumber.py
@@ -1,7 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, session
+import random
 
 randomnumberBlueprint = Blueprint("randomnumber",__name__)
 
 @randomnumberBlueprint.route("/",  methods=['GET'])
-def rmnum():
-    return "Nichts"
+#def rmnum():
+#    return "Nichts"
+
+
+def store_random_number( ):
+    session['RANDOM_NUMBER'] = get_random_number( )
+    return "Stored"
+
+def get_random_number( ): 
+    return random.randint(0, 100)
+
+    
+# Methode zum erzeugen der Random-Nummer 
+# -> Start des Prozesses, merken der zugehörigen Sessions 
+# session objekt der flask anwendung baut einen zufälligen key, mit diesem kann das frontend sich dann die Daten wiederholen 
+
+
+
+# Methode zum Prüfen der neu eingegebenen Nummer
+# -> Korrekt, zu groß, zu klein 
