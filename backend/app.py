789cca8952e0e4d80e5da51fb7a15875fcb2e6f6
DESKTOP-I1A08T7\Tanja Sauer <toblensauer@icloud.com>
2024-09-12 10:25:55 +0200

    DB erstellt

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes.randomnumber import randomnumberBlueprint  
+from routes.auth import authBlueprint 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "überlegdirwas"
@@ -9,7 +10,9 @@ app.config['SECRET_KEY'] = "überlegdirwas"
 
 
 
-app.register_blueprint(randomnumberBlueprint, url_prefix='/randomnumber')
 
+
+app.register_blueprint(randomnumberBlueprint, url_prefix='/randomnumber')
+app.register_blueprint(authBlueprint, url_prefix='/auth')
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ backend/routes/auth.py
@@ -0,0 +1,29 @@
+import sqlite3
+from flask import Blueprint, request
+
+authBlueprint = Blueprint("auth",__name__)
+
+@authBlueprint.route("/register", methods=['POST'])
+def register( ):
+    connection = get_db_connection()
+    cur = connection.cursor()
+    cur.execute("Insert into Users (Name, Created_At) Values(? , ?)",())
+    connection.commit()
+    connection.close()
+
+@authBlueprint.route("/login", methods=['POST'])
+def register( ):
+    connection = get_db_connection()
+    cur = connection.cursor()
+    username = request.args.get('User')
+    cur.execute("Select ID from Users where name = ?",(username))
+    row_id = cur.fetchall()
+    if row_id is None:
+        print('No such user')
+    else:
+        print()
+
+
+def get_db_connection(): 
+    connection = sqlite3.connect('zahlen-raten.db')
+    return connection
