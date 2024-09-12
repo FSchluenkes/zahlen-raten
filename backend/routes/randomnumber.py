caa0a92244bc1af9076a5049e93bf02ad2428f1b
rtintertbs1 <robintinter@gmx.de>
2024-09-12 14:13:38 +0200

    .

--- backend/routes/randomnumber.py
+++ backend/routes/randomnumber.py
@@ -3,17 +3,16 @@ import random
 from routes.db.connect_db import get_db_connection
 
 randomnumberBlueprint = Blueprint("randomnumber",__name__)
-#authBlueprint         = Blueprint("auth")
 
 def store_random_number( pd_random_number ):
     ld_connection = get_db_connection( )
     ld_cursor = ld_connection.cursor( )
     ld_cursor.execute("""INSERT INTO Statistik (tries, correct_number) VALUES (?, ?)""", (0, pd_random_number) )
-   # ld_row_id = ld_cursor.lastrowid( )
-   # if ld_row_id is not None:
-   #     ld_connection.commit( )
+    ld_session_id = ld_cursor.lastrowid 
+    if ld_session_id is not None:
+        ld_connection.commit()
      
-    return "test"
+    return ld_session_id
     
 def get_random_number( ): 
     return random.randint(0, 100)
@@ -28,11 +27,20 @@ def start_session( ):
 def new_guess( ):
     ld_guessed_number = request.args.get('guess') 
     ld_session_id     = request.args.get('session_id')
-    
-    #aus der db holen und vergleichen 
-    
-    return "ok"
-    
+
+    ld_connection = get_db_connection( )
+    ld_cursor = ld_connection.cursor( )
+    ld_cursor.execute("""SELECT Correct_Number FROM Statistik WHERE ID = :ld_session_id """,{ "ld_session_id":ld_session_id } )
+    ld_correct_number = ld_cursor.fetchall( )
+
+    #for zeile in ld_correct_number: 
+    #    if ld_guessed_number == zeile:
+    #        return "correct"
+    #    elif ld_guessed_number < zeile:
+    #        return "higher"
+    #    elif ld_guessed_number > :
+    #       return "lower"
+        
     
 # Methode zum erzeugen der Random-Nummer 
 # -> Start des Prozesses, merken der zugehörigen Sessions 
