4228f1539b6cda0b5b77e160b419377621b47b71
rtintertbs1 <robintinter@gmx.de>
2024-09-12 12:00:10 +0200

    Merge um die DB funktionen zu bekommen

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,10 @@
 from flask import Flask
 from routes.randomnumber import randomnumberBlueprint  
-from routes.auth import authBlueprint 
+#from routes.auth import authBlueprint 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "überlegdirwas"
-
-
-#eigene route für session handling?  
-
-
 
 app.register_blueprint(randomnumberBlueprint, url_prefix='/randomnumber')
-app.register_blueprint(authBlueprint, url_prefix='/auth')
+#app.register_blueprint(authBlueprint, url_prefix='/auth')
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/routes/randomnumber.py
+++ backend/routes/randomnumber.py
@@ -1,19 +1,35 @@
-from flask import Blueprint, session
+from flask import Blueprint, request
 import random
+import uuid
 #from routes.auth import authBlueprint 
 
 randomnumberBlueprint = Blueprint("randomnumber",__name__)
 #authBlueprint         = Blueprint("auth")
 
-@randomnumberBlueprint.route("/start_session",  methods=['GET'])
-def store_random_number( ):
+def store_random_number( pd_random_number ):
     #session id erzeugen  und mit random number speichern 
-    #dafür db tabelle anlegen 
-    print("Stored")
+    ld_session_id = uuid.uuid4()
+    return ld_session_id
+    
     
 def get_random_number( ): 
     return random.randint(0, 100)
 
+@randomnumberBlueprint.route("/start_session",  methods=['GET'])
+def start_session( ): 
+    ld_rndm_nmbr = get_random_number( )
+    ld_session_id = store_random_number(ld_rndm_nmbr)
+    return "Session erzeugt" + " " + str(ld_rndm_nmbr) + " " + str(ld_session_id)
+    
+@randomnumberBlueprint.route("/guess", methods=['GET'] )
+def new_guess( ):
+    ld_guessed_number = request.args.get('guess') 
+    ld_session_id     = request.args.get('session_id')
+    
+    #aus der db holen und vergleichen 
+    
+    return "ok"
+    
     
 # Methode zum erzeugen der Random-Nummer 
 # -> Start des Prozesses, merken der zugehörigen Sessions 
