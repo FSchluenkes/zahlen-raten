a6667350697d9d636db3a744860f0bff03b64ca6
FSchluenkes <frederik@schluenkes.de>
2024-09-19 10:21:24 +0200

    backend mit sqlalchemy und jwt

---
+++ backend/game.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request, jwt_required
+from models import Game, User
+
+game_bp = Blueprint('game', __name__)
+
+@game_bp.get('start')
+@jwt_required(optional=True)
+def start_game():
+    game: Game = Game()
+    
+    username = get_jwt_identity()
+    if username:
+        user: User = User.get_user_by_name(username)
+        if user.id:
+            game.user_id = user.id
+    
+    game.save()
+    return jsonify({"game_id": game.id}), 200
+
+@game_bp.post('guess')
+# @jwt_required(optional=True)
+def guess():
+    game_id:str = request.get_json().get('game_id')
+    number:int = int(request.get_json().get('number'))
+
+    if game_id:
+        try:
+            return Game.guess(game_id, number)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
+    else:
+        return jsonify({"error": "No game_id provided"}), 400   
--- backend/routes/db/connect_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import os
-
-def get_db_connection(): 
-    db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../db/zahlen-ratenDB.db"))
-    connection = sqlite3.connect(db_path)
-    return connection
